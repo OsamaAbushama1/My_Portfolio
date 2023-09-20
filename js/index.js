231e33fc0ee8ae0b107de8c438669272d37d5d33
OsamaAbushama <140832395+OsamaAbushama1@users.noreply.github.com>
2023-09-20 03:36:37 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,23 @@
+menubar = document.querySelector(".menubar");
+menubar.onclick = function(){
+    navbar = document.querySelector(".nav-bar")
+    navbar.classList.toggle("active")
+}
+
+
+window.addEventListener("scroll" , reveal);
+function reveal(){
+    var reveals=document.querySelectorAll(".reveal");
+    for (var i=0 ; i< reveals.length;i++){
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if (revealtop < windowheight - revealpoint){
+            reveals[i].classList.add('active')
+        }
+        else{
+            reveals[i].classList.remove('active')
+        }
+    }
+}
